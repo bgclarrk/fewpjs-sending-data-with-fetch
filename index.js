9efd9f2aa3d814736f39c4c7fc89e8e6d45bf823
+ <+>
2021-08-13 15:36:51 -0700

    Sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+
+    const submitData = {
+        name: userName,
+        email: userEmail
+    };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(submitData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        debugger;
+        const node = document.createElement("h1");
+        const nodeText = document.createTextNode(`${object.id}`);
+        node.appendChild(nodeText);
+        document.querySelector("body").appendChild(node);
+    })
+    .catch(function(error) {
+        const node = document.createElement("h1");
+        const nodeText = document.createTextNode(`${error.message}`);
+        node.appendChild(nodeText);
+        document.querySelector("body").appendChild(node);
+    });
+};
